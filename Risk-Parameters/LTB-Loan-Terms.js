a75ab79ddca650fabe1fffa861fd78cb03efddce
Ryan Berkun <45776981+rbcp18@users.noreply.github.com>
2020-04-20 13:03:33 -0700

    Update and rename risk.js to LTB-Loan-Terms.js

--- Risk-Parameters/LTB-Loan-Terms.js
+++ Risk-Parameters/LTB-Loan-Terms.js
@@ -5,38 +5,6 @@ const { decrypt }       = require('../encrypt-decrypt');
 const fetchTransactions = require('../utils/fetch-transactions');
 const findUserByWallet  = require('../utils/query-user');
 
-async function loanToBalance(wallet, accountId) {
-  // percent of balance to allow as collateral
-  const COLLATERAL_PERCENT = 0.2;
-
-  const user        = await findUserByWallet(wallet);
-  const accessToken = decrypt(user.plaid_access_token);
-
-  // Fetch account
-  const opts    = { account_ids: accountId };
-  const account = await plaid.client.getBalance(accessToken, opts);
-
-  // Fetch and save latest txns to ensure they're up-to-date
-  // positive transaction amounts = money leaving account,
-  // negative transaction amounts = money entering account
-  // Source: https://plaid.com/docs/#transactions
-  const txns = await fetchTransactions({ wallet, account });
-
-  let lowestBalance = account.balances.available;
-  let calculated    = lowestBalance;
-
-  // Working backwards from the current balance.
-  // Iterate through previous txns,
-  // If money left the account, add it back in,
-  // if money entered the account, remove it.
-  for (const txn of txns) {
-    calculated += txn.amount;
-    lowestBalance = Math.min(calculated, lowestBalance);
-  }
-
-  return lowestBalance * COLLATERAL_PERCENT;
-}
-
 /**
  * @api {post} / getLoanTerms
  * @apiVersion 1.0.0
